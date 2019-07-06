23f8f52a5698c2c88e98b3c66108fc14ea45bee3
arammuradyan <aram.muradyan2089@gmail.com>
2019-07-06 18:32:15 +0400

    test commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         Machine machine=Machine.getInstance();
-        showMenu(scanner,machine);
+        showMenu(scanner,machine );
     }
 
     public static void showMenu(Scanner scanner,Machine machine){
